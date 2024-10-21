0418290e66ba41fb44b8f1a5c1450e793b86ec8c
rattikabete <tahamo7amed81@gmail.com>
2024-10-20 21:18:04 -0700

    implemented a service for createProject & resolved converting DateTime to Timestamp

--- micro_project/src/project/project.module.ts
+++ micro_project/src/project/project.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProjectController } from './project.controller';
+import { ProjectService } from './project.service';
+import { UtilsService } from '@lib/utils/utils.service';
 
-@Module({})
+@Module({
+  providers: [ProjectService, UtilsService],
+  controllers: [ProjectController],
+})
 export class ProjectModule {}
